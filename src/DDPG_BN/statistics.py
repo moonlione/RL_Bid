3702f5982ac9836958df676d27a822328286262b
JiaXingBingGan <729869585@qq.com>
2019-11-04 10:01:49 +0800

    修改statistics.py

--- src/DDPG_BN/statistics.py
+++ src/DDPG_BN/statistics.py
@@ -15,6 +15,8 @@ def test_env(budget, test_data, eCPC, actions):
 
     e_bids = np.array([])
     e_market_prices = np.array([])
+    e_hours = np.array([])
+    e_real_labels = np.array([]) # 是否被点击
     # 状态包括：当前CTR，
     for t in range(24):
         auc_datas = test_data[test_data[:, config['data_hour_index']] == t]
@@ -24,6 +26,8 @@ def test_env(budget, test_data, eCPC, actions):
         bids = np.where(bids >= 300, 300, bids)
 
         market_prices = auc_datas[:, config['data_marketprice_index']]
+        hours = auc_datas[:, config['data_hour_index']]
+        real_labels = auc_datas[:, config['data_clk_index']]
         win_auctions = auc_datas[bids >= auc_datas[:, config['data_marketprice_index']]]
 
         e_cost[t] = np.sum(win_auctions[:, config['data_marketprice_index']])
@@ -43,6 +47,8 @@ def test_env(budget, test_data, eCPC, actions):
 
             bids = []
             market_prices = []
+            hours = []
+            real_labels = []
             for i in range(len(auc_datas)):
                 if temp_cost >= (budget - np.sum(e_cost[:t])):
                     break
@@ -59,6 +65,8 @@ def test_env(budget, test_data, eCPC, actions):
 
                 bids.append(bid)
                 market_prices.append(temp_market_price)
+                hours.append(current_data[config['data_hour_index']])
+                real_labels.append(temp_clk)
                 if bid > temp_market_price:
                     e_clks[t] += temp_clk
                     imps[t] += 1
@@ -69,15 +77,53 @@ def test_env(budget, test_data, eCPC, actions):
 
         e_bids = np.hstack((e_bids, bids))
         e_market_prices = np.hstack((e_market_prices, market_prices))
+        e_hours = np.hstack((e_hours, hours))
+        e_real_labels = np.hstack((e_real_labels, real_labels))
         if np.sum(e_cost) >= budget:
             break
 
-    print(e_market_prices.tolist())
-    print(e_bids.tolist(), e_clks, imps, bid_nums)
+    print(imps)
+    print(bid_nums)
+    print(e_clks, np.sum(e_clks))
+
+    records = {'bids': e_bids.tolist(), 'market_prices':e_market_prices.tolist(), 'clks': e_real_labels, 'hours': e_hours}
+    records_df = pd.DataFrame(data=records)
+    records_df.to_csv(directory + '/bids_' + str(config['budget_para'][0]) + '.csv', index=None)
+
+def max_train_index(directory, para):
+    train_results = pd.read_csv(directory + '/train_episode_results_' + str(para) + '.csv')
+    train_clks = train_results.values[:, [0, 5]]
+
+    test_results = pd.read_csv(directory + '/test_episode_results_' + str(para) + '.csv').values
+    test_clks = test_results[:, [0, 4]]
+
+    new_test_clks = []
+    for i in range(len(test_clks)):
+        test_clk_temp = [test_clks[i, 1] for k in range(10)]
+        new_test_clks.append(test_clk_temp)
+
+    extend_test_clks = np.array(new_test_clks).flatten()
+
+    max_value = train_clks[train_clks[:, 1].argsort()][-1, 1]
+    max_value_indexs = train_clks[train_clks[:, 1] == max_value]
+
+    max_test_value = []
+    max_test_value_index = []
+    for index in max_value_indexs:
+        max_test_value.append(extend_test_clks[int(index[0])])
+        max_test_value_index.append(int(index[0]))
+
+    test_value_max_index = np.argmax(max_test_value)  # max_test_value 最大值的索引
+
+    return max_test_value_index[test_value_max_index]
 
 directory = 'result_adjust_reward'
 action_file = directory + '/test_episode_actions_' + str(config['budget_para'][0]) + '.csv'
-actions = pd.read_csv(action_file, header=None).drop([0]).iloc[0, 1:].tolist() # 后面应该更新为选择最优的对应的actions
+actions_df = pd.read_csv(action_file, header=None).drop([0])
+
+max_result_index = max_train_index(directory, config['budget_para'][0])
+print(max_result_index)
+actions = actions_df.iloc[max_result_index, 1:].tolist()
 
 test_data = pd.read_csv('../../data/' + config['campaign_id'] + '/test_data.csv', header=None).drop([0])
 test_data.iloc[:, config['data_clk_index']:config['data_marketprice_index'] + 2] \
@@ -91,7 +137,7 @@ test_data = test_data.values
 budget = config['test_budget'] * config['budget_para'][0]
 
 eCPC = 60920.22773088766 # 每次点击花费
-    # 由启发式算法得到最优eCPC 1458-60920.22773088766,38767.41764692851,33229.21512593873, 22152.81008395915‬
-    # 3386-77901.22125145316‬,47939.21307781733,35954.409808363,23969.60653890866‬
+# 由启发式算法得到最优eCPC 1458-60920.22773088766,38767.41764692851,33229.21512593873, 22152.81008395915‬
+# 3386-77901.22125145316‬,47939.21307781733,35954.409808363,23969.60653890866‬
 
 test_env(budget, test_data, eCPC, actions)
